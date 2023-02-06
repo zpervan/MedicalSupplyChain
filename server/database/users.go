dc603f82de59ae791c211e0575aa7530188b50c8
Zvonimir Pervan <z.pervan65@gmail.com>
2023-02-06 14:12:37 +0100

    Implement user creation functionality on the server side

--- server/database/users.go
+++ server/database/users.go
@@ -1,22 +1,27 @@
 package database
 
-// Representation of a user in the database.
-// TODO: Should we use the protobuf struct instead?
-type User struct {
-	Id         string
-	Username   string
-	Password   string
-	Name       string
-	Surname    string
-	Role       string
-	Department string
-}
+import (
+	"errors"
+	ms "server/proto"
+	"time"
+)
 
 // Create a new user in the database if the user does not exist.
 //
 // Returns an error if the creation was not successful or the user already exists.
-func (d Database) CreateUser(newUser User) error {
-	// d.handler.Exec("INSERT INTO user_account VALUES")
+func (d Database) CreateUser(newUser *ms.User) error {
+	if newUser == nil {
+		return errors.New("cannot create user. reason: passed user data is empty")
+	}
+
+	_, err := d.handler.Exec("INSERT INTO user_account(username, password, name, surname, department, role, created_on) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+		newUser.Username, newUser.Password, newUser.Name, newUser.Surname, newUser.Department, newUser.Role, time.Now().Format(time.RFC1123))
+
+	if err != nil {
+		d.logger.Error("cannot create user. reason: " + err.Error())
+		return err
+	}
+
 	return nil
 }
 
@@ -30,7 +35,7 @@ func (d Database) FetchUsers() error {
 // Update the user with the specified username.
 //
 // Returns an error if the update was not successful.
-func (d Database) UpdateUser(user User) error {
+func (d Database) UpdateUser(user *ms.User) error {
 	return nil
 }
 
--- server/web/handlers.go
+++ server/web/handlers.go
@@ -9,14 +9,14 @@ import (
 )
 
 type Server struct {
-	Log *core.Logger
+	Log      *core.Logger
 	Database *database.Database
 	ms.UnimplementedMedicalSuppliesServer
 }
 
 func NewServer(log *core.Logger, database *database.Database) *Server {
 	server := &Server{}
-	
+
 	server.Log = log
 	server.Database = database
 
@@ -28,5 +28,16 @@ func (s *Server) TestRequest(ctx context.Context, in *ms.Request) (*ms.Response,
 
 	message := fmt.Sprintf("Hey, my dear friend %s", in.Data)
 
-	return &ms.Response{Data: message}, nil
+	return &ms.Response{Data: message, Error: &ms.Error{Code: ms.ErrorCode_OK, Message: ""}}, nil
+}
+
+func (s *Server) CreateUser(ctx context.Context, in *ms.User) (*ms.Response, error) {
+	s.Log.Info("creating new user")
+
+	err := s.Database.CreateUser(in)
+	if err != nil {
+		return &ms.Response{Error: &ms.Error{Code: ms.ErrorCode_INTERNAL_ERROR, Message: err.Error()}}, err
+	}
+	
+	return &ms.Response{}, nil
 }
